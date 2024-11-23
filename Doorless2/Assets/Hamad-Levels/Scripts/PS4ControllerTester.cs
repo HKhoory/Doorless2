8204cdc19f498793e50bcaf44bd174a9fa573e53
NullPerson <edu1khoory@hotmail.com>
2024-11-23 16:04:51 +0400

    Controller Setup Completed

--- Doorless2/Assets/Hamad-Levels/Scripts/PS4ControllerTester.cs
+++ Doorless2/Assets/Hamad-Levels/Scripts/PS4ControllerTester.cs
@@ -7,8 +7,7 @@ using TMPro;
 public class PS4ControllerTester : MonoBehaviour
 {
 
-    [SerializeField] private GameObject[] texts;
-    [SerializeField] private TMP_Text[] outputs;
+    [SerializeField] private TMP_Text outputs;
 
     //how about have them as static ints and another script prints the ints instead
     
@@ -20,19 +19,23 @@ public class PS4ControllerTester : MonoBehaviour
         //}
     }
 
-    
+
     void Update()
     {
-        
-        if (Input.GetKey(KeyCode.Joystick1Button0))
-        {
-            //outputs[0] = texts[0].GetComponent<TMP_Text>();
-            outputs[0].text = "X = 1";
-        }
-        else
-        {
-            outputs[0].text = "X = 0";
-        }
 
+        outputs.text = "X = " + Input.GetKey(KeyCode.Joystick1Button0) + "<br>"
+        + "Square = " + Input.GetKey(KeyCode.Joystick1Button1) + "<br>"
+        + "O = " + Input.GetKey(KeyCode.Joystick1Button2) + "<br>"
+        + "Triangle = " + Input.GetKey(KeyCode.Joystick1Button3) + "<br>"
+        + "L1 = " + Input.GetKey(KeyCode.Joystick1Button4) + "<br>"
+        + "L2 = " + Input.GetKey(KeyCode.Joystick1Button5) + "<br>"
+        + "R1 = " + Input.GetKey(KeyCode.Joystick1Button6) + "<br>"
+        + "R2 = " + Input.GetKey(KeyCode.Joystick1Button7) + "<br>"
+        + "D-Up = " + Input.GetKey(KeyCode.Joystick1Button8) + "<br>"
+        + "D-Down = " + Input.GetKey(KeyCode.Joystick1Button9) + "<br>"
+        + "D-Left = " + Input.GetKey(KeyCode.Joystick1Button10) + "<br>"
+        + "D-Right = " + Input.GetKey(KeyCode.Joystick1Button11) + "<br>"
+        + " = " + Input.GetKey(KeyCode.Joystick1Button12) + "<br>";
+        Input.GetAxisRaw("Horizontal");
     }
 }
