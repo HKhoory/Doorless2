87ebde64216ffd930e024945d6b936b06ad9b91a
NullPerson <edu1khoory@hotmail.com>
2024-11-25 21:59:18 +0400

    Made the new input system for the player

---
+++ Doorless2/Assets/NEW/Hamad/Hamad-Levels/PlayerMover.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMover : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody _rb;
+
+    [SerializeField] private float speed;
+    
+    [SerializeField] private Vector2 movementInput;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector3(movementInput.x * speed, 0 , movementInput.y * speed);
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+    public void OnMove(InputAction.CallbackContext x) => movementInput = x.ReadValue<Vector2>();
+
+}
