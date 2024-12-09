6b54dce6c695e94e43178241bd2f31904c52d80e
NullPerson <edu1khoory@hotmail.com>
2024-12-09 18:49:06 +0400

    Player Manager Refined

---
+++ Doorless2/Assets/NEW/Hamad/Hamad-Levels/Scripts/MainPlayerInput.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayerInput : MonoBehaviour
+{
+    
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
--- Doorless2/Assets/NEW/Hamad/Hamad-Levels/Scripts/PInputHandler.cs
+++ Doorless2/Assets/NEW/Hamad/Hamad-Levels/Scripts/PInputHandler.cs
@@ -2,24 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
 using static UnityEngine.InputSystem.InputAction;
+using System;
 
 public class PInputHandler : MonoBehaviour
 {
 
     private PlayerInput playerInput;
     private PlayerMover _playerMover;
+
     
 
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        
         playerInput = GetComponent<PlayerInput>();
         var playerMovers = FindObjectsOfType<PlayerMover>();
         var index = playerInput.playerIndex;
         _playerMover = playerMovers.FirstOrDefault(m => m.GetPlayerIndex() == index);
        
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (_playerMover == null)
+        {
+            Debug.Log("searching for player gameobjects");
+            Awake();
+            
+        }
     }
 
     public void OnMove(CallbackContext context)
@@ -28,5 +46,8 @@ public class PInputHandler : MonoBehaviour
         {
             _playerMover.SetInputVector(context.ReadValue<Vector2>());
         }
+        
     }
+
+    
 }
